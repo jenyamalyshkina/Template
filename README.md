dee29a8b419b032bdd64ce20195db442118e838f
jenyamalyshkina <evgeniyamalyshkina@gmail.com>
2016-02-29 12:26:31 +0700

    four

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 1. git clone https://github.com/jenyamalyshkina/Template Folder
 2. cd Folder
-3. rm -r .git
-4. bower i
-5. npm i
-6. gulp
+3. remove .git folder from project
+4. npm i
+5. gulp
--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,17 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
     spritesmith = require('gulp.spritesmith'),
+    eslint = require('gulp-eslint'),
+    through = require('gulp-through'),
+    notify = require('gulp-notify'),
     browserSync = require('browser-sync');
 
 
 // Компиляция файлов .jade
 gulp.task('jade-compile', function () {
   var YOUR_LOCALS = {};
-
   gulp.src('./dev/jade/pages/*.jade')
-    .pipe(plumber())
+    .pipe(plumber({errorHandler: notify.onError('Jade: <%= error.message %>')}))
     .pipe(jade({
       locals: YOUR_LOCALS,
       pretty: true
@@ -26,19 +28,23 @@ gulp.task('jade-compile', function () {
 // Компиляция и конкатенация файлов .scss
 gulp.task('sass-compile', function () {
   return gulp.src('dev/scss/*.scss')
-    .pipe(plumber())
+    .pipe(plumber({errorHandler: notify.onError("Sass: <%= error.message %>")}))
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(concat('main.min.css'))
     .pipe(sourcemaps.write())
+    .pipe(browserSync.stream())
     .pipe(gulp.dest('prod/css'));
 });
 
 
 // Минимификация и конкатенация файлов .js
 gulp.task('min-js', function() {
-  return gulp.src('dev/*.js')
+  return gulp.src(['bower/jquery/dist/jquery.js', 'bower/jquery-ui/jquery-ui.js', 'bower/jquery.columnizer/src/jquery.columnizer.js', 'bower/selectize/dist/js/standalone/selectize.js', 'dev/js/**/*.js'])
+    .pipe(plumber({errorHandler: notify.onError("JS: <%= error.message %>")}))
+    .pipe(eslint())
+    .pipe(eslint.format())
     .pipe(sourcemaps.init())
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -51,12 +57,26 @@ gulp.task('min-js', function() {
 gulp.task('sprite', function () {
   var spriteData = gulp.src('dev/img/icons/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
-    cssName: 'sprite.scss',
+    cssName: '_sprite.scss',
     padding: 10,
     algorithm: 'top-down'
   }));
     spriteData.img.pipe(gulp.dest('dev/img/sprite/'));
-    spriteData.css.pipe(gulp.dest('dev/scss/'));
+    spriteData.css.pipe(gulp.dest('dev/scss/_common/'));
+});
+
+
+//Перенос шрифтов в prod
+gulp.task('copy-fonts', function() {
+    gulp.src('dev/fonts/*.*')
+    .pipe(gulp.dest('prod/fonts'))
+});
+
+
+//Перенос картинок в prod
+gulp.task('copy-images', function() {
+    gulp.src(['dev/img/**/*.*', '!dev/img/icons/*.*'])
+    .pipe(gulp.dest('prod/img'))
 });
 
 
@@ -76,10 +96,11 @@ gulp.task('watch', function () {
   gulp.watch('dev/jade/**/*.jade', ['jade-compile']);
   gulp.watch('dev/scss/**/*.scss', ['sass-compile']);
   gulp.watch('dev/js/**/*.js', ['min-js']);
+  gulp.watch('dev/fonts/*.*', ['copy-fonts']);
+  gulp.watch('dev/img/**/*.*', ['copy-images']);
   gulp.watch([
     'prod/*.html',
-    'prod/js/**/*.js',
-    'prod/css/**/*.css'
+    'prod/js/**/*.js'
   ]).on('change', browserSync.reload);
 });
 
@@ -89,6 +110,8 @@ gulp.task('default', [
   'jade-compile',
   'sass-compile',
   'min-js',
+  'copy-fonts',
+  'copy-images',
   'server',
   'watch'
   ]);
